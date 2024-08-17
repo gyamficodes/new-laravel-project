02ff58ca38d2aaa891ee69128de7df70ba6490d0
gyamficodes <gyamfijohn08@gmail.com>
2024-08-17 10:43:51 +0000

    update

--- app/Models/job.php
+++ app/Models/job.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,34 +9,34 @@ use Illuminate\Database\Eloquent\Model;
 class job
 {
 
-    public static function all(): array {
+    public static function all(): array
+    {
         return  [
             [
                 "id" => 1,
                 "title" => "Director",
-                 "salary" => "$50,000"
+                "salary" => "$50,000"
             ],
             [
                 "id" => 2,
                 "title" => "Deveeloper",
-                 "salary" => "$20,000"
+                "salary" => "$20,000"
             ],
             [
                 "id" => 3,
                 "title" => "Teacher",
-                 "salary" => "$10,000"
+                "salary" => "$10,000"
             ],
         ];
-       }
+    }
 
-       public static function find(int $id): array  {
-     
-   $job = Arr::first(static::all(), fn($job) => $job["id"] === $id);
-    if(! $job){
-        abort(404);
-      }
- return $job;
-       }
+    public static function find(int $id): array
+    {
+
+        $job = Arr::first(static::all(), fn($job) => $job["id"] === $id);
+        if (! $job) {
+            abort(404);
+        }
+        return $job;
+    }
 }
-
-
