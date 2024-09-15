bfbb042286f52691a9c0d21e97cb396d9c9ca7a5
gyamficodes <gyamfijohn08@gmail.com>
2024-09-15 20:58:38 +0000

    learn how to create a new Job to My DB

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use App\Models\post;
 
 
 Route::get('/', function () {
-//   $jobs = job::all();
-//    $posts = post::all();
-//    dd($posts);
-//    dd($jobs);
-    return view('home' );
+    //   $jobs = job::all();
+    //    $posts = post::all();
+    //    dd($posts);
+    //    dd($jobs);
+    return view('home');
 });
 
 
@@ -18,15 +18,31 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     // $job = job::with("employer")->get();
-    $job = job::with("employer")->paginate(4);
-    return  view("jobs", ["jobs"=> $job]);
+    
+    // $job = job::with("employer")->paginate(4);
+    $job = job::with("employer")->latest()->paginate(4); // lates means that every job we create should push to fist.
+    return  view("jobs.index", ["jobs" => $job]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = job::find($id);
-    return view("job", ["job" => $job]);
+    return view("jobs.show", ["job" => $job]);
 });
 
+
+Route::post('/jobs', function () {
+    //   dd(request()->all());
+    Job::create([
+        "title" =>  request("title"),
+        "salary" => request("salary"),
+        "employer_id" => 1
+    ]);
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return  view("contact");
